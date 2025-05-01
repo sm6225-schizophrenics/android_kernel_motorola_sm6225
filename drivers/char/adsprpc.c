ba5d41b97bcb316e9cc27446e38a7bb741fb96bf
Muhammad Guntur <mochtur@gmail.com>
2025-05-02 04:36:17 +0700

    drivers: adsprpc: remove buf_size from fastrpc_set_process_info
    Unused after debugging removal
    
    this fixes:
    ../drivers/char/adsprpc.c:4291:15: warning: unused variable 'buf_size' [-Wunused-variable]
    4291 |         int err = 0, buf_size = 0;
         |                      ^~~~~~~~
    
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -4302,7 +4302,7 @@ static int fastrpc_device_open(struct inode *inode, struct file *filp)
 
 static int fastrpc_set_process_info(struct fastrpc_file *fl)
 {
-	int err = 0, buf_size = 0;
+	int err = 0;
 	char strpid[PID_SIZE];
 	char cur_comm[TASK_COMM_LEN];
 
