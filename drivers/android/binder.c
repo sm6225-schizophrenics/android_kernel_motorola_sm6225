6de54ff0f86140fc1cbde0ab1cb3170f8e0459b3
MatrixhKa <familiacomplicada1234@gmail.com>
2026-02-08 18:35:53 -0300

    Reapply "binder: fix max_thread type inconsistency"
    This reverts commit ac2ca37b1929a6539868310d237a0469e2c9ac26.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -569,7 +569,7 @@ struct binder_proc {
 	struct binder_stats stats;
 #endif
 	struct list_head delivered_death;
-	int max_threads;
+	u32 max_threads;
 	int requested_threads;
 	int requested_threads_started;
 	int tmp_ref;
@@ -5216,7 +5216,7 @@ static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			goto err;
 		break;
 	case BINDER_SET_MAX_THREADS: {
-		int max_threads;
+		u32 max_threads;
 
 		if (copy_from_user(&max_threads, ubuf,
 				   sizeof(max_threads))) {
