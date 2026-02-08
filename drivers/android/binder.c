02a19862ff4bb19934a68a40528fb31d8577b9ac
MatrixhKa <familiacomplicada1234@gmail.com>
2026-02-08 18:27:57 -0300

    Revert "binder: simplify the return expression of binder_mmap"
    This reverts commit d9e3c7a502cdc8baf8c2ad73f56338845c2951c4.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5136,7 +5136,9 @@ static const struct vm_operations_struct binder_vm_ops = {
 
 static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+	int ret;
 	struct binder_proc *proc = filp->private_data;
+	const char *failure_string;
 
 	if (proc->tsk != current->group_leader)
 		return -EINVAL;
@@ -5148,9 +5150,9 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 		     (unsigned long)pgprot_val(vma->vm_page_prot));
 
 	if (vma->vm_flags & FORBIDDEN_MMAP_FLAGS) {
-		pr_err("%s: %d %lx-%lx %s failed %d\n", __func__,
-		       proc->pid, vma->vm_start, vma->vm_end, "bad vm_flags", -EPERM);
-		return -EPERM;
+		ret = -EPERM;
+		failure_string = "bad vm_flags";
+		goto err_bad_arg;
 	}
 	vma->vm_flags |= VM_DONTCOPY | VM_MIXEDMAP;
 	vma->vm_flags &= ~VM_MAYWRITE;
@@ -5158,7 +5160,15 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	vma->vm_ops = &binder_vm_ops;
 	vma->vm_private_data = proc;
 
-	return binder_alloc_mmap_handler(&proc->alloc, vma);
+	ret = binder_alloc_mmap_handler(&proc->alloc, vma);
+	if (ret)
+		return ret;
+	return 0;
+
+err_bad_arg:
+	pr_err("%s: %d %lx-%lx %s failed %d\n", __func__,
+	       proc->pid, vma->vm_start, vma->vm_end, failure_string, ret);
+	return ret;
 }
 
 static int binder_open(struct inode *nodp, struct file *filp)
